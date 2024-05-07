77c7e8716ebb41683b8818e27610981c5eb6ea87
Caio Silva <stonerfuzz00@gmail.com>
2024-05-07 11:49:57 -0300

    Commit inicial do projeto

---
+++ caoculadora/caoculadora/caoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  caoculadoraApp.swift
+//  caoculadora
+//
+//  Created by Caio Silva on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct caoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
