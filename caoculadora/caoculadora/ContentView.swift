936f4acfebcc6f7f7edfcba01ffb67486fe61e44
Caio Silva <stonerfuzz00@gmail.com>
2024-05-09 11:52:26 -0300

    bufalo soldier - Implementaçao botao e clarinha

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
+    @State var result: Int?
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão?")
@@ -21,7 +22,22 @@ struct ContentView: View {
             TextField("E quantos meses alem disse ele tem",
                 value: $months,
                 format: .number)
+            Text("Porte")
+            //segmentcontrol
+            if let result { Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos ") }
+            else {
+                Image(ImageResource.clarinha).resizable().scaledToFit().frame(maxHeight: 150)
+                .frame(maxWidth: .infinity) }
             
+            Button("Cãocular") {
+                print("caocular")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .background(.indigo)
+            .foregroundStyle(.white)
+            .clipShape(.rect(cornerRadius: 10))
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
