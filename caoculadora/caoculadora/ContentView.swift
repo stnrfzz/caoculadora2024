b743cadacb14c45f4bfad77f6d9ffcb5f36ea4ea
Caio Silva <stonerfuzz00@gmail.com>
2024-05-14 09:12:37 -0300

    Extracao funcao e navigation stack

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -17,42 +17,45 @@ struct ContentView: View {
      
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Spacer()
-            Text("Qual a idade do seu cão?").font(.header5)
-            Spacer()
-            Text("Anos").font(.body1)
-            TextField("Quantos anos completos tem seu cão",
-                value: $years,
-                format: .number )
-            Text("Meses").bold()
-            TextField("E quantos meses alem disse ele tem",
-                value: $months,
-                format: .number)
-            Divider()
-            Text("Porte").bold()
-            
-            Picker("Portes", selection: $porteSelecionado) {
-                ForEach(Portes.allCases, id: \.self) { porte in Text(porte.rawValue.capitalized)}
-            }.pickerStyle(.segmented)
-            Spacer()
-            if let result { Text("Seu cachorro tem, em idade humana...")
-                Text("\(result) anos ") }
-            else {
-                Image(ImageResource.clarinha).resizable().scaledToFit().frame(maxHeight: 250)
-                .frame(maxWidth: .infinity).shadow(radius: 50) }
-            Spacer()
-            Button("Cãocular", action: processYears)
-            .frame(maxWidth: .infinity)
-            .frame(height: 50)
-            .background(.indigo)
-            .foregroundStyle(.white)
-            .clipShape(.rect(cornerRadius: 10))
-            Spacer()
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("Qual a idade do seu cão?").font(.header5)
+                Spacer()
+                Text("Anos").font(.body1)
+                TextField("Quantos anos completos tem seu cão",
+                          value: $years,
+                          format: .number )
+                Text("Meses").bold()
+                TextField("E quantos meses alem disse ele tem",
+                          value: $months,
+                          format: .number)
+                Divider()
+                Text("Porte").bold()
+                
+                Picker("Portes", selection: $porteSelecionado) {
+                    ForEach(Portes.allCases, id: \.self) { porte in Text(porte.rawValue.capitalized)}
+                }.pickerStyle(.segmented)
+                Spacer()
+                if let result { Text("Seu cachorro tem, em idade humana...")
+                    Text("\(result) anos ") }
+                else {
+                    Image(ImageResource.clarinha).resizable().scaledToFit().frame(maxHeight: 250)
+                    .frame(maxWidth: .infinity).shadow(radius: 50) }
+                Spacer()
+                Button("Cãocular", action: processYears)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(.indigo)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+                Spacer()
+            }
+            .textFieldStyle(.roundedBorder)
+            .keyboardType(.numberPad)
+            .padding()
+            .navigationTitle("Cãoculadora")
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
     }
     func processYears() {
         print("cãocular")
@@ -67,23 +70,34 @@ struct ContentView: View {
             return
         }
         
-        let multiplicador: Int
+        result = porteSelecionado.calcularIdade(aCaninos: years
+                                        , mCaninos: months)
         
-        switch porteSelecionado {
-        case .pequeno:
-            multiplicador = 6
-        case .medio:
-            multiplicador = 7
-        case .grande:
-            multiplicador = 8
-        }
-        result = years * multiplicador + months * multiplicador / 12
     }
     
     enum Portes: String, CaseIterable {
         case pequeno = "Pequeno"
         case medio = "Médio"
         case grande = "Grande"
+        
+        func calcularIdade(aCaninos aCaninos: Int, mCaninos mCaninos: Int) -> Int {
+            
+            let multiplicador: Int
+            let resultado: Int
+            
+            switch self {
+            case .pequeno:
+                multiplicador = 6
+            case .medio:
+                multiplicador = 7
+            case .grande:
+                multiplicador = 8
+            }
+            resultado = aCaninos * multiplicador + mCaninos * multiplicador / 12
+            
+            return resultado
+        }
+            
     }
     
 }
